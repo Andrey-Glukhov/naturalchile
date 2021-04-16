719434dc86ab39a4d7afad002efcc25b96975e0a
Eliza <gluhova.liza@gmail.com>
2021-04-16 13:17:01 +0300

    small

--- contact.php
+++ contact.php
@@ -6,7 +6,13 @@ get_header(); ?>
 <?php $q_home = new WP_Query( array( 'page_id' => 10 ) );
 
 if ($q_home -> have_posts() ) : while ( $q_home -> have_posts() ) : $q_home -> the_post(); ?>
+
 <div class="container-fluid home-container">
+    <div class="row">
+        <div class="col-12">
+            <h1><?php the_title(); ?></h1>
+        </div>
+    </div>
     <div class="row">
         <div class="col-md-2 col-sm-1 col-1">
 
