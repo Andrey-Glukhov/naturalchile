38651574f0e834f8329b4c7d7e0266fe8e8ae571
Eliza <gluhova.liza@gmail.com>
2021-04-17 12:01:21 +0300

    contact

--- contact.php
+++ contact.php
@@ -15,13 +15,20 @@ if ($q_home -> have_posts() ) : while ( $q_home -> have_posts() ) : $q_home -> t
         </div>
     </div>
     <div class="row">
-        <div class="col-md-2 col-sm-1 col-1">
+        <div class="col-md-2 col-sm-1 col-1 contact-grid-left">
+        <div class="contact-cell-left-1 bg_color_black" ></div>
+        <div class="contact-cell-left-2 bg_color_red" ></div>
+        <div class="contact-cell-left-3 bg_color_white"></div>
+        <div class="contact-cell-left-4 bg_color_rose"></div>
+        <div class="contact-cell-left-5 bg_color_green"></div>
+        <div class="contact-cell-left-6 bg_color_green"></div>
+        <div class="contact-cell-left-7 bg_color_blue"></div>
 
         </div>
         <div class="col-md-8 col-sm-10 col-10 contact_formcol">
             <?php the_content(); ?>
         </div>
-        <div class="col-md-2 col-sm-1 col-1">
+        <div class="col-md-2 col-sm-1 col-1 contact-grid-right">
 
         </div>
     </div>
