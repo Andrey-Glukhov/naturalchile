37168d7b0ef867d9e5b9e5d44c8ae21a3bff6495
Andrey-Glukhov <agi2000@mail.ru>
2021-04-26 12:13:25 +0300

    sincronize

--- js/n_chile.js
+++ js/n_chile.js
@@ -4,4 +4,26 @@ jQuery(function($) {
         $('[name="update_cart"]').trigger('click');
     });
 
+    var $quantityArrowMinus = $(".add_quantity_wrapper .var_quantity-arrow-minus");
+    var $quantityArrowPlus = $(".add_quantity_wrapper .var_quantity-arrow-plus");
+    
+ 
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+ 
+    function quantityMinus(event) {
+     var $quantityNum = $(this).parent().find('.quantity input[type="number"]'); 
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+      event.preventDefault();
+      //event.stopProragation();
+    }
+ 
+    function quantityPlus(event) {
+      var $quantityNum = $(this).parent().find('.quantity input[type="number"]');
+      $quantityNum.val(+$quantityNum.val() + 1);
+      event.preventDefault();
+      //event.stopProragation();
+    }
 });
