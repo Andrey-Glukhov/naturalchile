5e16e4fd8138c752824f7f8660f3c108b9c11bd0
Andrey-Glukhov <agi2000@mail.ru>
2021-04-26 19:57:58 +0300

    change animation

--- js/n_chile.js
+++ js/n_chile.js
@@ -30,10 +30,24 @@ jQuery(function($) {
 });
 // $(window).on('load', function() {
       //window.load = function() {
-        window.addEventListener('load', function () {     
+        window.addEventListener('load', setGrid, false);
+        window.addEventListener('resize', setGrid, false);
+        var gridTime1 = gsap.timeline({repeat: -1, repeatDelay: 1, yoyo: 1});
+        var gridTime2 = gsap.timeline({repeat: -1, repeatDelay: 2, yoyo: 1});
+        var animate1 = ['upper-cell-2', 'upper-cell-10'];
+        var animate2 = ['upper-cell-13'];
+        var animate3 = ['upper-cell-4', 'upper-cell-16'];
+        var animate4 = ['upper-cell-34'];
+        function setGrid() {     
           var gridBlock = document.querySelector('.home-grid-wraper');
           var gridCells = gridBlock.querySelectorAll('.home-grid-wraper>*');
-          
+          if (document.querySelector('.upper_home_grid')) {
+            //console.log('++++');
+            document.querySelector('.upper_home_grid').remove();
+            gridTime1.clear();
+            gridTime2.clear();
+            //return;
+          }
           var  divNewGrid = document.createElement('div');
           divNewGrid.classList.add('upper_home_grid');
 
@@ -41,13 +55,38 @@ jQuery(function($) {
           gridCells.forEach(function(divChild ) {
             //console.log(divChild );
             var newCell =  divChild.cloneNode(true);
-            var newClass = '';
+            var newClassColor = '';
+            var newClassNumber = '';
             for (var name of newCell.classList) {
-              if (name.inludes('bg_color')) {
-                newClass = name;
+              if (name.includes('bg_color')) {
+                newClassColor = name;
+              }
+              if (name.includes('home-cell')) {
+                if (name.match(/\d+/)[0]){
+                  newClassNumber = 'upper-cell-' + name.match(/\d+/)[0] ;
+                }  
               }
             }
-            console.log(newCell.classList);//document.createElement('div');
+            newCell.className = newClassColor + ' ' + newClassNumber;
+            newCell.style.width = divChild.offsetWidth +'px';
+            newCell.style.height = divChild.offsetHeight + 'px';
+            newCell.style.top = divChild.offsetTop +'px';
+            newCell.style.left = divChild.offsetLeft + 'px'
+           // console.log(newCell.classList);//document.createElement('div');
             divNewGrid.append(newCell);
+            if (animate1.includes(newClassNumber)) {
+              newCell.style.zIndex = '10';
+              gridTime1.to(newCell, {width: 2* divChild.offsetWidth, duration: 3});
+            } else if (animate2.includes(newClassNumber)) {
+              newCell.style.zIndex = '10';
+              gridTime1.to(newCell, {height: 2* divChild.offsetHeight, duration: 3});
+            } else if (animate3.includes(newClassNumber)) {
+              newCell.style.zIndex = '10';
+              gridTime2.to(newCell, {height: 2* divChild.offsetHeight, duration: 3});
+            } else if (animate4.includes(newClassNumber)) {
+              newCell.style.zIndex = '10';
+              gridTime2.to(newCell, {height: 2* divChild.offsetHeight, duration: 3});
+            }
           });
-      }, false);
+
+      }
